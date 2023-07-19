6a68bb7277da4e0171b472c95d92a9b8ac51564c
Aserturik <alex.hernandez27@uptc.edu.co>
2023-07-19 16:34:49 -0500

    User-contacts relation

--- login.php
+++ login.php
@@ -41,7 +41,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
 <?php require "partials/header.php" ?>
 
-<div class="container pt-5">
+<div class="container pt-5">|
   <div class="row justify-content-center">
     <div class="col-md-8">
       <div class="card">
